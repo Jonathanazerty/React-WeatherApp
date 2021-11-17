9a7a8e83e6cb3a75df00dd55b256b483156e89d8
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-11-17 14:41:11 +0100

    add async function with data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-export const Home = () => {
+const Home = () => {
+    async function weatherInfo() {
+        try {
+            const data = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            const weatherData = await data.json();
+            console.log(weatherData);
+            console.log(data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
     return (
         <div>
             <div>
                 <h1>
                     Weather app
+                    <button onClick={weatherInfo}>Submit</button>
                 </h1>
             </div>
         </div>
