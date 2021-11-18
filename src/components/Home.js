1764e1156dd4ad0e5eb9cb23c70b44d7851c9ac1
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-11-18 18:10:11 +0100

    display information

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,49 @@ import React, {useState} from 'react'
 
 const Home = () => {
 
-     const [input, setInput] = useState(" ");
+     const [city, setCity] = useState("");
+    //  const [temperature, setTemperature] = useState("");
+    const [weather, setWeather] = useState([{}]);
     
     async function weatherInfo() {
         try {
-            const data = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            const data = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
             const weatherData = await data.json();
-            console.log(weatherData);
-            console.log(input);
+            setWeather(weatherData)
+            console.log(weatherData.city.name)
+            console.log(weatherData.city.country)
+            console.log(weatherData.list[0].main.temp)
+            console.log(weatherData.list[0].weather[0].description)
         }
         catch(err){
             console.log(err);
         }
     }
+    console.log(weather)
+    const nameCity = weather.city.name;
+    const nameCountry = weather.city.country;
+    const temperature = weather.list[0].main.temp;
+    const description = weather.list[0].weather[0].description;
     return (
+
         <div>
-            <input  className="input" type="text" name="input" placeholder="Which city / country" value={input} onChange={(e) => setInput(e.target.value)}/><br></br>
-            <button className="submit" onClick={weatherInfo}>Submit</button>
+            <input  
+                className="input" 
+                type="text" 
+                name="input" 
+                placeholder="Which city / country" 
+                value={city} 
+                onChange={(e) => setCity(e.target.value)}
+                /><br></br>
+                <button className="submit" onClick={weatherInfo}>Submit</button>
+            <div onClick={weatherInfo}>
+                {nameCity}<br></br>
+                {nameCountry}<br></br>
+                {Math.round(temperature)}°C<br></br>
+                {description}<br></br>
+            </div>
         </div>
+
     )
 }
 
