5f23c2f61ab0e5587fa50dd480f5b15ecd3d2a33
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-11-18 10:02:36 +0100

    add input data of city

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {  BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {  BrowserRouter, Routes, Route /*, Link*/} from 'react-router-dom';
 import Home from './components/Home';
 import Search from './components/Search';
 import Location from './components/Location';
@@ -17,10 +17,10 @@ function App() {
     <>
     <BrowserRouter>
         <h3 className="todaysdate"> {today} </h3>
-          <Link className="Link" to="/">Home</Link>
+          {/* <Link className="Link" to="/">Home</Link>
           <Link className="Link" to="Search">Search</Link>
           <Link className="Link" to="Location">Location</Link>
-          <Link className="Link" to="OtherDays">Other days</Link>
+          <Link className="Link" to="OtherDays">Other days</Link> */}
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="Search" element={<Search />}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Home = () => {
+
+     const [input, setInput] = useState(" ");
+    
     async function weatherInfo() {
         try {
-            const data = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            const data = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
             const weatherData = await data.json();
             console.log(weatherData);
-            console.log(data);
+            console.log(input);
         }
         catch(err){
             console.log(err);
@@ -14,12 +17,8 @@ const Home = () => {
     }
     return (
         <div>
-            <div>
-                <h1>
-                    Weather app
-                    <button onClick={weatherInfo}>Submit</button>
-                </h1>
-            </div>
+            <input  className="input" type="text" name="input" placeholder="Which city / country" value={input} onChange={(e) => setInput(e.target.value)}/><br></br>
+            <button className="submit" onClick={weatherInfo}>Submit</button>
         </div>
     )
 }
